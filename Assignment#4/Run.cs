37e1e96072efba8a8bd60b2f6e5f6e9637b3c66b
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2025-02-09 04:52:31 -0500

    Commenting in run class

--- Assignment#4/Run.cs
+++ Assignment#4/Run.cs
@@ -15,23 +15,29 @@ using System.Threading.Tasks;
 
 namespace Assignment_4
 {
+    /// <summary>
+    /// Class called Run with only one method Logic to check logic of the program
+    /// </summary>
     internal class Run
     {
-
-        public void Logic()
+        /// <summary>
+        /// It contains the logic of the program. First it sets all the data members of 5 members of class MemberOfSocialMedia and 
+        /// also creates a UI class to display output by calling the GetName and CalculateAge of MemberOfSocialMedia class. 
+        /// </summary>
+        static public void Logic()
         {
-            UI ui = new UI(); 
+            UI ui = new UI();   //Istantination of a UI class
             
             MemberOfSocialClub[] memberOfSocialClubs = new MemberOfSocialClub[5];
 
 
-            for (int i = 0; i < memberOfSocialClubs.Length; i++) 
+            for (int i = 0; i < memberOfSocialClubs.Length; i++)        //loop to create and also set 5 member's data
             {
                 memberOfSocialClubs[i] = new MemberOfSocialClub();
                 memberOfSocialClubs[i].SetAllData(((i * i * i * 8) - (i * i)), $"Hell{i}o", $"W{i}orld", $"{i}HelloWorld.{i}@gmail.com", new DateTime(2001 + i, 02, 14));
             }
 
-            foreach (var memberOfSocialClub in memberOfSocialClubs) 
+            foreach (var memberOfSocialClub in memberOfSocialClubs) //loop to display output from each member from the array of 5
             {
                 
                 ui.Display($"Member's Name: {memberOfSocialClub.GetName()}");
