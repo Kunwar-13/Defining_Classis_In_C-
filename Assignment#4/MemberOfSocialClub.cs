b3ef937603639094c662a1513765a519ea581539
Ayushpreet Singh <Asingh82295@conestogac.on.ca>
2025-02-09 05:48:45 -0500

    Commenting in MemberOfSocialClub class

--- Assignment#4/MemberOfSocialClub.cs
+++ Assignment#4/MemberOfSocialClub.cs
@@ -15,14 +15,29 @@ using System.Threading.Tasks;
 
 namespace Assignment_4
 {
+    /// <summary>
+    /// A class that represents a member in a social club. 
+    /// It has 5 public data members. A method to set values of all the data members.
+    /// A method GetName to get the first and last name of the member.
+    /// A method to get the member's age.
+    /// </summary>
     public class MemberOfSocialClub
     {
+        //Data Mambers
         public int MemberId;
         public string FirstName;
         public string LastName;
         public string EmailAddress;
         public DateTime DateOfBirth;
 
+        /// <summary>
+        /// Sets values of all data members based on their corresponding counterpart in the parameters
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="firstname"></param>
+        /// <param name="lastname"></param>
+        /// <param name="emailaddress"></param>
+        /// <param name="dob"></param>
         public void SetAllData(int id, string firstname, string lastname, string emailaddress, DateTime dob)
         {
             MemberId = id;
@@ -32,12 +47,20 @@ namespace Assignment_4
             DateOfBirth = dob;
         }
 
+        /// <summary>
+        /// It returns the name of member in {Lastname, Firstname} style
+        /// </summary>
+        /// <returns>String= L.n, F.n</returns>
         public string GetName()
         {
             return $"{LastName}, {FirstName}";
         }
 
 
+        /// <summary>
+        /// It calculates the age only based on years in years
+        /// </summary>
+        /// <returns>int = age(in years)</returns>
         public int CalculateAge()
         {
             if (DateOfBirth < DateTime.Now)
